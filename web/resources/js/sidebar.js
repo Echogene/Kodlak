6711733a5af8dcac58e4eafac7124479b4886187
Echogene <echogene.alpha@gmail.com>
2014-07-05 18:00:33 +0100

    Add initial sidebar classes

---
+++ web/resources/js/sidebar.js
@@ -0,0 +1,49 @@
+/**
+ * @constructor
+ * @implements Control
+ */
+function Sidebar() {
+	/**
+	 * @type {Array.<SidebarSection>}
+	 * @private
+	 */
+	this._sections = [];
+}
+
+Sidebar.prototype.create = function() {
+	var control = $('<div/>').addClass('sidebar control');
+	this._control = control;
+
+	$.each(this._sections, function() {
+		// Here, "this" is the section over which we are iterating.
+		//noinspection JSPotentiallyInvalidUsageOfThis
+		control.append(this.create());
+	});
+
+	return control;
+};
+
+
+/**
+ * Add a section to the sidebar and visually add it if the sidebar has been drawn.
+ * @param {SidebarSection} sidebarSection
+ */
+Sidebar.prototype.addSection = function(sidebarSection) {
+	this._sections.push(sidebarSection);
+	if (this._control) {
+		this._control.append(sidebarSection.create());
+	}
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+/**
+ * @interface
+ * @extends Control
+ */
+function SidebarSection() {
+	Control.call(this);
+}
+
+SidebarSection.prototype = Object.create(Control.prototype);
+SidebarSection.prototype.constructor = SidebarSection;
