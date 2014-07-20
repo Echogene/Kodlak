e78ffe053c0e402901793060534376f95dea1292
Echogene <echogene.alpha@gmail.com>
2014-07-20 23:31:46 +0100

    Keep on documenting, slowly but surely

--- web/resources/js/role.js
+++ web/resources/js/role.js
@@ -246,16 +246,28 @@ AddRoleControl.prototype.create = function() {
 	return control;
 };
 
+/**
+ * Change the mode (edit or read-only) of this control.  This is done by changing the class name of the main container,
+ * which changes the visibility of the sub controls (relying on the CSS).
+ * @param {string} mode 'edit' for edit mode, 'read' for read-only mode
+ * @private
+ */
 AddRoleControl.prototype._updateMode = function(mode) {
 	this._control.removeClass(this._mode);
 	this._mode = mode;
 	this._control.addClass(this._mode);
 };
 
+/**
+ * @inheritDoc
+ */
 AddRoleControl.prototype.cancel = function() {
 	this._updateMode('read');
 };
 
+/**
+ * Call the onSuccess function but continue in edit mode so you can keep adding roles.
+ */
 AddRoleControl.prototype.finish = function() {
 	var roleName = this._addRoleInput.val().trim().toLowerCase();
 	if (this._onSuccess && roleName !== '') {
@@ -264,6 +276,9 @@ AddRoleControl.prototype.finish = function() {
 	this._addRoleInput.select();
 };
 
+/**
+ * Flash red to say that the role could not be added.  This is because the server could not accept the name.
+ */
 AddRoleControl.prototype.fail = function() {
 	flashBackground(this._addRoleInput, '#f03020');
 };
