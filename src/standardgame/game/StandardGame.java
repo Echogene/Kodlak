e67ae4de7d3f8c5bfe84fac508bbe6dc0de2cd53
Echogene <echogene.alpha@gmail.com>
2014-09-06 13:19:40 +0100

    Remove stuff

--- src/standardgame/game/StandardGame.java
+++ src/standardgame/game/StandardGame.java
@@ -1,19 +1,12 @@
 package standardgame.game;
 
 import model.game.AbstractGame;
-import model.message.MessageSender;
 import org.springframework.beans.factory.annotation.Autowired;
-import server.components.messagesender.LoggedMessageSender;
-import server.components.messagesender.LoggedMessageSenderFactory;
 import standardgame.alignment.VillagerWerewolfAlignment;
-import standardgame.alignment.VillagerWerewolfAlignmentResolver;
 import standardgame.phase.DayNightPhase;
 import standardgame.player.StandardPlayer;
 import standardgame.player.status.StandardStatus;
 import standardgame.role.StandardRole;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static standardgame.phase.DayNightPhase.Phase.NIGHT;
 
@@ -23,18 +16,8 @@ import static standardgame.phase.DayNightPhase.Phase.NIGHT;
 public class StandardGame
 		extends AbstractGame<DayNightPhase, VillagerWerewolfAlignment, StandardStatus, StandardRole, StandardPlayer> {
 
-	private final VillagerWerewolfAlignmentResolver resolver = new VillagerWerewolfAlignmentResolver();
-	private final LoggedMessageSenderFactory messageSenderFactory;
-	private final Map<StandardPlayer, LoggedMessageSender> messageSenders = new HashMap<>();
-
 	@Autowired
-	public StandardGame(LoggedMessageSenderFactory messageSenderFactory) {
-
+	public StandardGame() {
 		super(new DayNightPhase(NIGHT));
-		this.messageSenderFactory = messageSenderFactory;
-	}
-
-	public MessageSender getMessageSender(StandardPlayer player) {
-		return messageSenders.get(player);
 	}
 }
