bb4d59ce6938380a013d177228a02ab79d569921
Echogene <echogene.alpha@gmail.com>
2014-09-06 13:35:09 +0100

    Start to observe the start of the night

--- src/standardgame/game/StandardGame.java
+++ src/standardgame/game/StandardGame.java
@@ -4,11 +4,14 @@ import model.game.AbstractGame;
 import org.springframework.beans.factory.annotation.Autowired;
 import standardgame.alignment.VillagerWerewolfAlignment;
 import standardgame.phase.DayNightPhase;
+import standardgame.phase.PhaseChangeEvent;
 import standardgame.player.StandardPlayer;
 import standardgame.player.status.StandardStatus;
 import standardgame.role.StandardRole;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import static standardgame.phase.DayNightPhase.Phase.NIGHT;
+import static standardgame.phase.PhaseStartEvent.NIGHT_START;
 
 /**
  * @author Steven Weston
@@ -19,5 +22,10 @@ public class StandardGame
 	@Autowired
 	public StandardGame() {
 		super(new DayNightPhase(NIGHT));
+		currentPhase.observe(NIGHT_START, this::onNightStart);
+	}
+
+	private void onNightStart(PhaseChangeEvent phaseChangeEvent) {
+		throw new NotImplementedException();
 	}
 }
